7d96462148e90100c2c827f5269aa0b08ae63874
vikramdev772 <vikramdev772@gmail.com>
2026-08-03 15:36:21 +0530

    react router 🔩

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import Main from "./pages/Main";
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
