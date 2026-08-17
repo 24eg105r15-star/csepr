08b9d9ed7bb9be61db8eb7d2c73a166e6e79e0e7
vikramdev772 <vikramdev772@gmail.com>
2026-08-17 11:47:55 +0530

    added delete api

--- backend/cse/src/main/java/jar/controller/Delete.java
+++ backend/cse/src/main/java/jar/controller/Delete.java
@@ -1,29 +1,25 @@
 package jar.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jar.repo.StudentRepo;
+import jar.services.Dservice;
 
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin("*")
 public class Delete {
+    Dservice obj;
+    Delete(Dservice obj){
+        this.obj=obj;
 
-    @Autowired
-    StudentRepo db;
-
+    }
     @DeleteMapping("/{id}")
     public String deleteStudent(@PathVariable long id) {
-        if (db.existsById(id)) {
-            db.deleteById(id);
-            return "Student deleted successfully";
-        } else {
-            return "Student not found";
-        }
+        return obj.f(id);
     }
+
 }
